e55ed8390ee02faa8b4c61159f9ac7bc5df5f8e3
Forc3ment <kperigault@gmail.com>
2017-01-30 16:56:50 +0100

    many things ...

--- code/tp/binary_desc.cpp
+++ code/tp/binary_desc.cpp
@@ -1,32 +1,31 @@
 #include <visp/vpConfig.h>
 #include <visp/vpDebug.h>
 #include "affiche_image.cpp"
+#include <visp3/gui/vpPlot.h>
 
 using namespace std;
 
-void lbp(vpImage<unsigned char>& im, vpImage<int>& i1, vpImage<int>& i2, vpImage<int>& i3, vpImage<int>& i4, 
-	vpImage<int>& i5, vpImage<int>& i6, vpImage<int>& i7, vpImage<int>& i8)
+void lbp(vpImage<unsigned char>& im, vpImage<bool>& i1, vpImage<bool>& i2, vpImage<bool>& i3, vpImage<bool>& i4, 
+	vpImage<bool>& i5, vpImage<bool>& i6, vpImage<bool>& i7, vpImage<bool>& i8)
 {
 	for (int i = 1; i < im.getHeight()-1; i++)
 		for (int j = 1; j < im.getWidth()-1; j++) {
-			i1[i][j] = (im[i-1][j-1] < im[i][j])*255;
-			i2[i][j] = (im[i-1][j] < im[i][j])*255;
-			i3[i][j] = (im[i-1][j+1] < im[i][j])*255;
-			i4[i][j] = (im[i][j-1] < im[i][j])*255;
-			i5[i][j] = (im[i][j+1] < im[i][j])*255;
-			i6[i][j] = (im[i+1][j-1] < im[i][j])*255;
-			i7[i][j] = (im[i+1][j] < im[i][j])*255;
-			i8[i][j] = (im[i+1][j+1] < im[i][j])*255;
+			i1[i][j] = im[i-1][j-1] < im[i][j];
+			i2[i][j] = im[i-1][j] < im[i][j];
+			i3[i][j] = im[i-1][j+1] < im[i][j];
+			i4[i][j] = im[i][j-1] < im[i][j];
+			i5[i][j] = im[i][j+1] < im[i][j];
+			i6[i][j] = im[i+1][j-1] < im[i][j];
+			i7[i][j] = im[i+1][j] < im[i][j];
+			i8[i][j] = im[i+1][j+1] < im[i][j];
 		}
 }
 
-int patch_errors(vpImage<unsigned char>& i1, vpImage<unsigned char>& i2)
+int patch_errors(vpImage<bool>& i11, vpImage<bool>& i12, vpImage<bool>& i13, vpImage<bool>& i14, 
+	vpImage<bool>& i15, vpImage<bool>& i16, vpImage<bool>& i17, vpImage<bool>& i18, vpImage<bool>& i21, vpImage<bool>& i22, vpImage<bool>& i23, vpImage<bool>& i24, 
+	vpImage<bool>& i25, vpImage<bool>& i26, vpImage<bool>& i27, vpImage<bool>& i28)
 {	
-	int h = i1.getHeight(), w = i1.getWidth();
-	vpImage<int> i11(h,w,0), i12(h,w,0), i13(h,w,0), i14(h,w,0), i15(h,w,0), i16(h,w,0), i17(h,w,0), i18(h,w,0);
-	vpImage<int> i21(h,w,0), i22(h,w,0), i23(h,w,0), i24(h,w,0), i25(h,w,0), i26(h,w,0), i27(h,w,0), i28(h,w,0);
-	lbp(i1, i11, i12, i13, i14, i15, i16, i17, i18);
-	lbp(i2, i21, i22, i23, i24, i25, i26, i27, i28);
+	int h = i11.getHeight(), w = i11.getWidth();
 	int error = 0;
 	for (int i = 0; i < h; i++)
 		for (int j = 0; j < w; j++) {
@@ -42,26 +41,65 @@ int patch_errors(vpImage<unsigned char>& i1, vpImage<unsigned char>& i2)
 	return error;
 }
 
-void process_graph(vpImage<unsigned char>& im, int hpatch, int wpatch, vpImage<unsigned char>& graph)
+void process_graph(vpImage<unsigned char>& im, int hpatch, int wpatch, vpImage<double>& graph)
 {
+	int h = im.getHeight(), w = im.getWidth();
+
+	vpImage<bool> i1(h,w,0), i2(h,w,0), i3(h,w,0), i4(h,w,0), i5(h,w,0), i6(h,w,0), i7(h,w,0), i8(h,w,0);
+	lbp(im, i1, i2, i3, i4, i5, i6, i7, i8);
+
+	vpImage<bool> fixed_p1(hpatch,wpatch,0), fixed_p2(hpatch,wpatch,0), fixed_p3(hpatch,wpatch,0), fixed_p4(hpatch,wpatch,0)
+				, fixed_p5(hpatch,wpatch,0), fixed_p6(hpatch,wpatch,0), fixed_p7(hpatch,wpatch,0), fixed_p8(hpatch,wpatch,0);
+
+	vpImage<bool> p1(hpatch,wpatch,0), p2(hpatch,wpatch,0), p3(hpatch,wpatch,0), p4(hpatch,wpatch,0)
+				, p5(hpatch,wpatch,0), p6(hpatch,wpatch,0), p7(hpatch,wpatch,0), p8(hpatch,wpatch,0);
+
 	int hpatch2 = hpatch/2, wpatch2 = wpatch/2;
+	
 	// create & init central fixed patch
-	vpImage<unsigned char> fixed_patch(hpatch,wpatch,0);
 	for (int i = 0; i < hpatch; i++)
+	{
 		for (int j = 0; j < wpatch; j++)
-			fixed_patch[i][j] = im[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+		{
+			fixed_p1[i][j] = i1[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p2[i][j] = i2[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p3[i][j] = i3[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p4[i][j] = i4[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p5[i][j] = i5[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p6[i][j] = i6[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p7[i][j] = i7[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+			fixed_p8[i][j] = i8[(im.getHeight()-hpatch)/2 + i][(im.getWidth()-wpatch)/2 + j];
+		}
+	}			
+	
+
 	// create moving patch
 	vpImage<unsigned char> moving_patch(hpatch,wpatch,0);
 	for (int i = hpatch2; i < im.getHeight()-hpatch2; i++)
-		for (int j = wpatch2; j < im.getWidth()-wpatch2; j++) {
+	{
+
+		for (int j = wpatch2; j < im.getWidth()-wpatch2; j++) 
+		{
 			// fill moving patch
 			for (int ii = 0; ii < hpatch; ii++)
-				for (int jj = 0; jj < wpatch; jj++) {
-					moving_patch[ii][jj] = im[i-hpatch2+ii][j-wpatch2+jj];
+			{
+				for (int jj = 0; jj < wpatch; jj++) 
+				{
+					p1[ii][jj] = i1[i-hpatch2+ii][j-wpatch2+jj];
+					p2[ii][jj] = i2[i-hpatch2+ii][j-wpatch2+jj];
+					p3[ii][jj] = i3[i-hpatch2+ii][j-wpatch2+jj];
+					p4[ii][jj] = i4[i-hpatch2+ii][j-wpatch2+jj];
+					p5[ii][jj] = i5[i-hpatch2+ii][j-wpatch2+jj];
+					p6[ii][jj] = i6[i-hpatch2+ii][j-wpatch2+jj];
+					p7[ii][jj] = i7[i-hpatch2+ii][j-wpatch2+jj];
+					p8[ii][jj] = i8[i-hpatch2+ii][j-wpatch2+jj];
 				}
-			// fill graph
-			graph[i][j] = patch_errors(fixed_patch, moving_patch);
+				// fill graph
+				graph[i][j] = patch_errors(fixed_p1,fixed_p2,fixed_p3,fixed_p4,fixed_p5,fixed_p6,fixed_p7,fixed_p8,
+										p1,p2,p3,p4,p5,p6,p7,p8);
+			}
 		}
+	}
 }
 
 int main(int argc, char **argv)
@@ -73,20 +111,31 @@ int main(int argc, char **argv)
 	
 	string im;
 	unsigned int h, w;	
-	vpImage<unsigned char>  I1;
+	vpImage<unsigned char>  I0;
 	
 	im = "../image/baboon_grey.ppm";
 
-	vpImageIo::read(I1, im);
-	vpImage<unsigned char> I2(I1.getHeight(), I1.getWidth());
-	//afficheImage(I1, 100, 100, "");
-	h=I1.getHeight(); w=I1.getWidth();
-	vpImage<int> i1(h,w,0), i2(h,w,0), i3(h,w,0), i4(h,w,0), i5(h,w,0), i6(h,w,0), i7(h,w,0), i8(h,w,0);
-	lbp(I1, i1, i2, i3, i4, i5, i6, i7, i8);
-	
-	process_graph(I1, 100, 100, I2);
+	vpImageIo::read(I0, im);
+	vpImage<double> result(I0.getHeight(), I0.getWidth(), 0);
+	vpImage<unsigned char> result_gray(I0.getHeight(), I0.getWidth(), 0);
 
-	afficheImage(I2, 100, 100, "");
+	//afficheImage(I1, 100, 100, "");
+	h=I0.getHeight(); w=I0.getWidth();
+
+	
+	process_graph(I0, 30, 30, result);
+
+  	// vpPlot A(1, 700, 700, 100, 100, "");
+  	// A.initGraph(0,1);
+
+  	//for (int i = 0; i < h; i++)
+		//std::cout<< result[i][100] << std::endl;
+
+	std::cout<< result[128][128] << std::endl;
+
+  	vpImageConvert::convert(result,result_gray);
+
+	afficheImage(result_gray, 100, 100, "");
 
 	cout << "Fin du programme " << endl;
 	return(0);
